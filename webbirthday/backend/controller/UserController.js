53d4a2a7af4cc8026b657754a9f0cadac848dd88
Dao Cong Pha <mongpha90@gmail.com>
2025-09-19 15:06:38 +0700

    Routes

---
+++ webbirthday/backend/controller/UserController.js
@@ -0,0 +1,10 @@
+// controllers/UserController.js
+const UserController = {
+  // [GET] /user
+  getUser: (req, res) => {
+    console.log(req.query); // ví dụ /user?id=123
+    res.send("User data");
+  },
+};
+
+export default UserController;
